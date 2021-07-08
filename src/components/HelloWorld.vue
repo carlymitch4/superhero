f4cecfd899f5e35aa6e960f62df35ca1e4ed626f
Carly Mitchell <carly.mitchell@encore.ca>
2021-07-08 13:15:13 -0400

    add grid

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <div
-      v-for="(superhero, index) in superheros" :key='index'
-      class="text-blue-500"
-    >
-      {{superhero.id}} {{superhero.name}}
+    <div class="text-3xl pb-12 font-hero">Superheros</div>
+    <div class="flex justify-center">
+      <div class="grid grid-cols-3 gap-4">
+        <div
+          v-for="(superhero, index) in superheros" :key='index'
+          class="rounded-lg bg-gray-500 w-80"
+        >
+          {{superhero.name}}
+          <img class="rounded-lg" :src="superhero.images.sm">
+        </div>
+      </div>
     </div>
   </div>
 </template>
