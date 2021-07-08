b5aa9142405d57499f27ca4f9e6f412f58b079bb
Carly Mitchell <carly.mitchell@encore.ca>
2021-07-08 12:45:48 -0400

    Superhero API added

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div
+      v-for="(superhero, index) in superheros" :key='index'
+      class="text-blue-500"
+    >
+      {{superhero.id}} {{superhero.name}}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'HelloWorld',
+  mounted () {
+    this.$store.dispatch('getSuperheros')
+  },
+  computed: {
+    ...mapState(['superheros'])
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex);
+
+const state = {
+  superheros: []
+}
+
+const getters = {}
+
+const actions = {
+  getSuperheros({ commit }) {
+    axios.get('https://akabab.github.io/superhero-api/api/all.json')
+      .then(response => {
+        commit('SET_SUPERHEROS', response.data)
+    })
+  }
+}
+
+const mutations = {
+    SET_SUPERHEROS(state, superheros) {
+        state.superheros = superheros
+    }
+}
+
+export default new Vuex.Store({
+    state,
+    getters,
+    actions,
+    mutations
+})
