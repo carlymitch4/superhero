43d57fc03b92b4ad54c29ec5530dd050f2f3a461
Carly Mitchell <carly.mitchell@encore.ca>
2021-07-19 16:52:21 -0400

    back to home and scroll fix

--- src/Superhero.vue
+++ src/Superhero.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="wrapper p-10">
-    <div>Superhero: {{ superheros[superheroIndex].name }}</div>
-    <div class="text-black">Strength: {{ superheros[superheroIndex].powerstats.strength }}</div>
-    <div class="text-black">Combat: {{ superheros[superheroIndex].powerstats.combat }}</div>
-    <div class="text-black">Power: {{ superheros[superheroIndex].powerstats.power }}</div>
-    <div class="text-black">Durability: {{ superheros[superheroIndex].powerstats.durability }}</div>
-    <div class="text-black">Speed: {{ superheros[superheroIndex].powerstats.speed }}</div>
-    <div class="text-black">Intelligence: {{ superheros[superheroIndex].powerstats.intelligence }}</div>
+  <div>
+    <div class="text-lg flex justify-center pt-10">
+      <router-link
+        to="/"
+      >
+        <span class="material-icons transform hover:-translate-x-1">arrow_back</span>
+      </router-link>
+    </div>
+    <div class="wrapper p-10">
+      <div>Superhero: {{ superheros[superheroIndex].name }}</div>
+      <div class="text-black">Strength: {{ superheros[superheroIndex].powerstats.strength }}</div>
+      <div class="text-black">Combat: {{ superheros[superheroIndex].powerstats.combat }}</div>
+      <div class="text-black">Power: {{ superheros[superheroIndex].powerstats.power }}</div>
+      <div class="text-black">Durability: {{ superheros[superheroIndex].powerstats.durability }}</div>
+      <div class="text-black">Speed: {{ superheros[superheroIndex].powerstats.speed }}</div>
+      <div class="text-black">Intelligence: {{ superheros[superheroIndex].powerstats.intelligence }}</div>
+    </div>
   </div>
 </template>
 
